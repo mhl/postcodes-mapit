c5d692aae2d3881cf6a0ee338d3e473fbb992544
Mark Longair <mhl@pobox.com>
2021-06-14 17:23:19 +0100

    Switch to calculating the Voronoi per region (ease memory use)

--- mapit_postcodes/management/commands/mapit_postcodes_populate_voronoi_table.py
+++ mapit_postcodes/management/commands/mapit_postcodes_populate_voronoi_table.py
@@ -40,88 +40,96 @@ class Command(BaseCommand):
     def handle(self, **options):
         required_pc_prefix = options["startswith"]
 
-        positions_list = []
-        position_to_row_ids = defaultdict(set)
+        # We do a Voronoi diagram for each region separately, since doing
+        # the whole of GB in one go takes way too much memory, even for a
+        # 64GB machine. (This is bad for the very few postcodes that cross
+        # EU region boundaries, but I can live with that for the moment.)
 
-        # Get the unique positions from the mapit_postcodes_nsulrow table
-        # into a list, storing the corresponding primary key of all the rows
-        # that refer to that position.
+        for region_code in NSULRow.objects.values_list("region_code", flat=True).distinct():
+            print("===== Processing region", region_code)
 
-        rows_processed = 0
-        for nsul_row in NSULRow.objects.all().iterator(chunk_size=BATCH_SIZE):
-            position_tuple = (int(nsul_row.point.x), int(nsul_row.point.y))
-            positions_list.append(position_tuple)
-            rows_processed += 1
-            if (rows_processed % 100000) == 0:
-                print(f"Read {rows_processed} rows from the database")
-            if required_pc_prefix and not nsul_row.startswith(required_pc_prefix):
-                continue
-            position_to_row_ids[position_tuple].add(nsul_row.id)
+            positions_list = []
+            position_to_row_ids = defaultdict(set)
 
-        # Now add some "points at infinity" - 200 points in a circle way
-        # outside the border of the United Kingdom:
+            # Get the unique positions from the mapit_postcodes_nsulrow table
+            # into a list, storing the corresponding primary key of all the rows
+            # that refer to that position.
 
-        points_at_infinity = 200
+            rows_processed = 0
+            for nsul_row in NSULRow.objects.filter(region_code=region_code).iterator(chunk_size=BATCH_SIZE):
+                position_tuple = (int(nsul_row.point.x), int(nsul_row.point.y))
+                positions_list.append(position_tuple)
+                rows_processed += 1
+                if (rows_processed % 100000) == 0:
+                    print(f"{region_code}: Read {rows_processed} rows from the database")
+                if required_pc_prefix and not nsul_row.startswith(required_pc_prefix):
+                    continue
+                position_to_row_ids[position_tuple].add(nsul_row.id)
 
-        distance_to_infinity = (UK_MAX_NORTHINGS - UK_MIN_NORTHINGS) * 1.5
+            # Now add some "points at infinity" - 200 points in a circle way
+            # outside the border of the United Kingdom:
 
-        for i in range(0, points_at_infinity):
-            angle = (2 * math.pi * i) / float(points_at_infinity)
-            new_x = CENTRE_OF_GB_E + math.cos(angle) * distance_to_infinity
-            new_y = CENTRE_OF_GB_N + math.sin(angle) * distance_to_infinity
-            positions_list.append((new_x, new_y))
+            points_at_infinity = 200
 
-        points = np.array(positions_list)
-        print("Calculating the Voronoi diagram...")
-        vor = Voronoi(points)
-        print("Finished!")
+            distance_to_infinity = (UK_MAX_NORTHINGS - UK_MIN_NORTHINGS) * 1.5
 
-        # Now put the Voronoi polygons into the database, and set up foreign keys
-        # from the NSUL rows. Batch them up so that we can use bulk_create and
-        # bulk_update.
+            for i in range(0, points_at_infinity):
+                angle = (2 * math.pi * i) / float(points_at_infinity)
+                new_x = CENTRE_OF_GB_E + math.cos(angle) * distance_to_infinity
+                new_y = CENTRE_OF_GB_N + math.sin(angle) * distance_to_infinity
+                positions_list.append((new_x, new_y))
 
-        total_positions = len(positions_list)
-        with tqdm(total=total_positions) as progress:
-            for start_index in range(0, total_positions, BATCH_SIZE):
-                n = min(BATCH_SIZE, total_positions - start_index)
-                print("Processing batch from index", start_index, "to", start_index + n - 1, "inclusive")
+            points = np.array(positions_list)
+            print(f"{region_code}: Calculating the Voronoi diagram...")
+            vor = Voronoi(points)
+            print(f"{region_code}: Finished!")
 
-                nr_list = []
-                vr_to_create = []
-                for i in range(start_index, start_index + n):
-                    position_tuple = positions_list[i]
-                    row_ids = position_to_row_ids[position_tuple]
-                    if not row_ids:
-                        # This is one of the "points at infinity" - ignore them
-                        continue
+            # Now put the Voronoi polygons into the database, and set up foreign keys
+            # from the NSUL rows. Batch them up so that we can use bulk_create and
+            # bulk_update.
 
-                    voronoi_region_index = vor.point_region[i]
-                    voronoi_region = vor.regions[voronoi_region_index]
-                    if any(vi < 0 for vi in voronoi_region):
-                        # Then this region extends to infinity, so is outside our "points at infinity"
-                        continue
-                    if len(voronoi_region) < 3:
-                        # Skip any point with fewer than 3 triangle_indices
-                        return
+            total_positions = len(positions_list)
+            with tqdm(total=total_positions) as progress:
+                for start_index in range(0, total_positions, BATCH_SIZE):
+                    n = min(BATCH_SIZE, total_positions - start_index)
+                    # print(f"{region_code}: Processing batch from index", start_index, "to", start_index + n - 1, "inclusive")
 
-                    border = [vor.vertices[i] for i in voronoi_region]
-                    border.append(border[0])
-                    # The coordinates are NumPy arrays, so convert them to tuples:
-                    border = [tuple(p) for p in border]
-                    polygon = Polygon(border, srid=27700)
+                    nr_list = []
+                    vr_to_create = []
+                    for i in range(start_index, start_index + n):
+                        position_tuple = positions_list[i]
+                        row_ids = position_to_row_ids[position_tuple]
+                        if not row_ids:
+                            # This is one of the "points at infinity" - ignore them
+                            continue
 
-                    voronoi_region_object = VoronoiRegion(polygon=polygon)
-                    vr_to_create.append(voronoi_region_object)
+                        voronoi_region_index = vor.point_region[i]
+                        voronoi_region = vor.regions[voronoi_region_index]
+                        if any(vi < 0 for vi in voronoi_region):
+                            # Then this region extends to infinity, so is outside our "points at infinity"
+                            continue
+                        if len(voronoi_region) < 3:
+                            # Skip any point with fewer than 3 triangle_indices
+                            return
 
-                    nsul_rows = [NSULRow.objects.get(pk=row_id) for row_id in row_ids]
-                    nr_list.append(nsul_rows)
+                        border = [vor.vertices[i] for i in voronoi_region]
+                        border.append(border[0])
+                        # The coordinates are NumPy arrays, so convert them to tuples:
+                        border = [tuple(p) for p in border]
+                        polygon = Polygon(border, srid=27700)
 
-                nr_to_update = []
-                vr_created = VoronoiRegion.objects.bulk_create(vr_to_create)
-                for i, voronoi_region in enumerate(vr_created):
-                    for nsul_row in nr_list[i]:
-                        nsul_row.voronoi_region = voronoi_region
-                        nr_to_update.append(nsul_row)
+                        voronoi_region_object = VoronoiRegion(polygon=polygon)
+                        vr_to_create.append(voronoi_region_object)
 
-                NSULRow.objects.bulk_update(nr_to_update, ["voronoi_region"])
-                progress.update(n)
+                        nsul_rows = [NSULRow.objects.get(pk=row_id) for row_id in row_ids]
+                        nr_list.append(nsul_rows)
+
+                    nr_to_update = []
+                    vr_created = VoronoiRegion.objects.bulk_create(vr_to_create)
+                    for i, voronoi_region in enumerate(vr_created):
+                        for nsul_row in nr_list[i]:
+                            nsul_row.voronoi_region = voronoi_region
+                            nr_to_update.append(nsul_row)
+
+                    NSULRow.objects.bulk_update(nr_to_update, ["voronoi_region"])
+                    progress.update(n)
