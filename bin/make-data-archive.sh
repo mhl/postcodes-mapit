5651c8ef55f52a14de49c5a9d7f16c152a1b735f
Mark Longair <mhl@pobox.com>
2021-08-22 14:55:46 +0100

    Update the script for making a publishable archive of the data

--- bin/make-data-archive.sh
+++ bin/make-data-archive.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" != 1 ]
 then
-    echo "Usage: $0 POSTCODE-KML-DIRECTORY"
+    echo "Usage: $0 POSTCODE-GEOJSON-DIRECTORY"
     exit 1
 fi
 
@@ -14,7 +14,8 @@ then
     exit 1
 fi
 
-OUTPUT_FILENAME="$(readlink -f "$D/../voronoi-of-onspd-kml.tar.bz2")"
+OUTPUT_BASENAME="$(basename $D)"
+OUTPUT_FILENAME="$(readlink -f "$D/../$OUTPUT_BASENAME.tar.bz2")"
 
 if [ -e "$OUTPUT_FILENAME" ]
 then
@@ -27,42 +28,44 @@ echo Generating "$OUTPUT_FILENAME"
 # Create a README and license file in that directoory:
 
 cat <<EOF > "$D/README"
-This archive contains very approximate boundaries for postcodes in the
-UK. For more information about how these were generated, see the blog
+This archive contains approximate boundaries for postcodes in Great
+Britain. For more information about how this data generated, see the blog
 post here:
 
-  https://longair.net/blog/2017/07/10/approximate-postcode-boundaries/
+  https://longair.net/blog/2021/08/20/open-data-gb-postcode-unit-boundaries
 
-This archive contains KML files for postcode areas, districts, sectors
-and units. Under the postcodes subdirectory, which contains the
-postcode unit KML files, there are also a few .json files that list
-the other postcodes which had the same coordinates in the source data
-(ONSPD) in the case that there was more than one.
+This archive contains GeoJSON files for postcode areas, districts,
+sectors and units. (The postcode unit boundaries are collected into
+a file per district.)
 
 The LICENSE file in this directory explains the conditions under which
 you can use this data.
+
+Contact: mark-postcodes@longair.net
 EOF
 
 cat <<EOF > "$D/LICENSE"
-The data in this archive is derived from the ONSPD, which is licensed
-under the Open Government License and the UK Government Licensing
-Framework. It contains National Statistics data © Crown copyright and
-database right 2017 and OS data © Crown copyright and database right
-2017. It is also partially derived from data from OpenStreetMap which
-is licensed under the Open Data Commons Open Database License by the
-OpenStreetMap Foundation (OSMF), created by OpenStreetMap and its
-contributors. You must abide by the terms of these licenses. These
-boundaries are known to be approximate and not accurate - they are
-provided in case they are of interest and use, but are only rough
-representations of the postcode boundaries due to the lack of open
-address data for the UK.
+The data in this archive is derived from NSUL (published by the ONS)
+and Boundary-Line (published by Ordnance Survey), which are both
+licensed under the Open Government License version 3. You must abide
+by the terms of these licenses and include the following copyright
+notices:
+
+Contains OS data © Crown copyright and database right 2020
+Contains Royal Mail data © Royal Mail copyright and database right 2020
+Source: Office for National Statistics licensed under the Open Government Licence v.3.0
+
+These boundaries are known to be approximate - they are provided in case
+they are of interest and use, but are only rough representations of the
+postcode boundaries due to limitations of the open address data currently
+available. In other words, there are no guarantees about the quality of
+this data or its suitability for any use; use it at your own risk.
 EOF
 
 # Now tar it up:
 D_WITHOUT_SLASH="${D%/}"
 
 
-tar  --transform "s|$D|voronoi-of-onspd-kml|" \
-    --sort=name \
+tar --sort=name \
     -cjvf "$OUTPUT_FILENAME" \
     "$D"
